b7a4f4b89fabc21aa5e01d4e23a07ccc5b280024
Spacecowboi <cunningham2192@gmail.com>
2023-07-14 14:26:47 -0700

    completes test for multiply

--- js/app.js
+++ js/app.js
@@ -9,13 +9,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-    let result = a + b;
-    let cat = 'The sum of ' + a + ' and ' + b + ' is ' + result + '.';
-    return [result, cat];
+  let result = a + b;
+  let cat = 'The sum of ' + a + ' and ' + b + ' is ' + result + '.';
+  return [result, cat];
 }
 
 // Here is the test for sum(); uncomment it to run it
- //testSum(4, 7);
+//testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -29,10 +29,12 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  let product = a * b;
+  let mCat = 'The product of ' + a + ' and ' + b + ' is ' + product + '.';
+  return [product, mCat];
 }
 
-// Here is the test for multiply(); uncomment it to run it
+//  Here is the test for multiply(); uncomment it to run it
 // testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
